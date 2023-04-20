f7074d557793999521ac3e73f199a211d68810cf
Evgeny Shmakov <sjs-master@yandex.ru>
2023-04-20 22:38:28 +0700

    add project

---
+++ src/components/icon/lib/index.ts
@@ -0,0 +1,13 @@
+import {notFound} from './404';
+import {call} from './call';
+import {flags} from './flags';
+import {telegram} from './telegram';
+import {whatsApp} from './whatsApp';
+
+export default {
+  flags,
+  notFound,
+  call,
+  whatsApp,
+  telegram,
+};
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export {Button} from './button';
+export {Label, Section,Text} from './common';
+export {Contact} from './contact';
+export {ContactForm} from './contactForm';
+export {Header} from './header';
+export {InputPhone} from './inputPhone';
+export {Intro} from './intro';
+export {Page} from './page';
+export {Select} from './select';
