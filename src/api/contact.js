67f46d59969007b45c9456fecf5f059bca131eac
Evgeny Shmakov <sjs-master@yandex.ru>
2023-08-06 22:34:41 +0700

    feat: remove project

--- src/api/contact.js
+++
@@ -1,3 +0,0 @@
-import {API} from './axios';
-
-export const contact = data => API.post('contact', data);
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export {Button} from './button';
-export {Label, Section,Text} from './common';
-export {Contact} from './contact';
-export {ContactForm} from './contactForm';
-export {Header} from './header';
-export {InputPhone} from './inputPhone';
-export {Intro} from './intro';
-export {Page} from './page';
-export {Select} from './select';
